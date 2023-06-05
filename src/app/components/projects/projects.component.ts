b3244ff78c97c3638b90ccc7d321113c75956e8e
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-06-05 23:08:55 +0530

    project page template ready

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.sass']
+  styleUrls: ['./projects.component.sass'],
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'Juicy-N-Yummy',
+      link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
+      src: 'https://plus.unsplash.com/premium_photo-1682308427166-2e5debb54179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+      title: 'Juicy-N-Yummy',
+      link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
+      src: 'https://plus.unsplash.com/premium_photo-1682308427166-2e5debb54179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+      title: 'Juicy-N-Yummy',
+      link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
+      src: 'https://plus.unsplash.com/premium_photo-1682308427166-2e5debb54179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+      title: 'Juicy-N-Yummy',
+      link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
+      src: 'https://plus.unsplash.com/premium_photo-1682308427166-2e5debb54179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+    {
+      title: 'Juicy-N-Yummy',
+      link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
+      src: 'https://plus.unsplash.com/premium_photo-1682308427166-2e5debb54179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    },
+  ];
 }
