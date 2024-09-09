5356f26122bd5b38dc40f2b24855bffaf5bf027f
prakhar-5447 <sahuprakhar022003@gmail.com>
2024-09-10 00:08:08 +0530

    fix cname

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,25 +11,25 @@ export class ProjectsComponent {
       title: 'Juicy-N-Yummy',
       github_link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
       link: 'https://pratham-0094.github.io/Juicy-N-Yummy',
-      src: '/portfolio/assets/images/projects/juicy-n-yummy.png',
+      src: '/assets/images/projects/juicy-n-yummy.png',
     },
     {
       title: 'Eventflow',
       github_link: 'https://github.com/prakhar-5447/eventflow',
       link: 'https://raw.githubusercontent.com/prakhar-5447/eventflow/main/eventflow.apk',
-      src: '/portfolio/assets/images/projects/eventflow.png',
+      src: '/assets/images/projects/eventflow.png',
     },
     {
       title: 'Airmeal',
       github_link: 'https://github.com/prakhar-5447/airmeal',
       link: 'https://prakhar-5447.github.io/airmeal',
-      src: '/portfolio/assets/images/projects/airmeal.png',
+      src: '/assets/images/projects/airmeal.png',
     },
     {
       title: 'Motion.io',
       github_link: 'https://github.com/prakhar-5447/motion_io',
       link: 'https://prakhar-5447.github.io/motion_io',
-      src: '/portfolio/assets/images/projects/motion_io.png',
+      src: '/assets/images/projects/motion_io.png',
     },
   ];
 
