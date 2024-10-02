7c1730bbe606521ea1cfccaaea855258e54b18dc
prakhar-5447 <sahuprakhar022003@gmail.com>
2024-10-02 20:34:45 +0530

    improve performance

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -6,33 +6,6 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./projects.component.sass'],
 })
 export class ProjectsComponent {
-  projects = [
-    {
-      title: 'Juicy-N-Yummy',
-      github_link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
-      link: 'https://pratham-0094.github.io/Juicy-N-Yummy',
-      src: '/assets/images/projects/juicy-n-yummy.png',
-    },
-    {
-      title: 'Eventflow',
-      github_link: 'https://github.com/prakhar-5447/eventflow',
-      link: 'https://raw.githubusercontent.com/prakhar-5447/eventflow/main/eventflow.apk',
-      src: '/assets/images/projects/eventflow.png',
-    },
-    {
-      title: 'Airmeal',
-      github_link: 'https://github.com/prakhar-5447/airmeal',
-      link: 'https://prakhar-5447.github.io/airmeal',
-      src: '/assets/images/projects/airmeal.png',
-    },
-    {
-      title: 'Motion.io',
-      github_link: 'https://github.com/prakhar-5447/motion_io',
-      link: 'https://prakhar-5447.github.io/motion_io',
-      src: '/assets/images/projects/motion_io.png',
-    },
-  ];
-
   @ViewChild('slidesContainer', { static: true }) slidesContainer!: ElementRef;
 
   scroll() {
