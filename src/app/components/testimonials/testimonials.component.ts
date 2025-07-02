0777a51062c2a15c3d87d7fa2f001389f3d0562b
Prakhar Sahu <sahuprakhar022003@gmail.com>
2025-07-02 16:44:33 +0530

    fix assets for build

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -13,19 +13,19 @@ export class TestimonialsComponent {
       quote: "Working with this team was an absolute pleasure. They delivered outstanding results that exceeded our expectations. The team was incredibly responsive and understood our needs perfectly. We couldn't be happier with the outcome.",
       name: "John Doe",
       role: "CEO, Company XYZ",
-      image: "/images/home/header.jpeg"
+      image: "./images/home/header.jpeg"
     },
     {
       quote: "Their attention to detail and commitment to quality is unparalleled. We highly recommend their services. Working with this team was an absolute pleasure. They delivered outstanding results that exceeded our expectations.",
       name: "Jane Smith",
       role: "Product Manager, ABC Corp",
-      image: "/images/home/header.jpeg"
+      image: "./images/home/header.jpeg"
     },
     {
       quote: "The team was incredibly responsive and understood our needs perfectly. We couldn't be happier with the outcome. Their attention to detail and commitment to quality is unparalleled. We highly recommend their services.",
       name: "Michael Lee",
       role: "CTO, Startup Inc.",
-      image: "/images/home/header.jpeg"
+      image: "./images/home/header.jpeg"
     },
   ];
 
